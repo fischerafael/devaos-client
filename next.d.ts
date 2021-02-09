e8ef8811acfaf2175462f9ab27ad609711fdd301
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-02-09 16:05:30 -0300

    removed index

---
+++ next.d.ts
@@ -0,0 +1,36 @@
+interface IPersonal {
+    avatar: string
+    location: string
+    title: string
+    name: string
+}
+
+interface IBio {
+    bio: string
+}
+
+interface ISKills {
+    _id: string
+    title: string
+    description?: string
+}
+
+interface IExp {
+    _id: string
+    type: string
+    title: string
+    institution: string
+    location: string
+    startedAt?: number
+    finishedAt?: number
+    description?: string
+}
+
+interface NextProps {
+    github: string
+    email: string
+    personal?: IPersonal
+    bio?: IBio
+    skills?: ISKills[]
+    experiences?: IExp[]
+}
