a6e1d38930d7d3e7fa612a2050418ac46577c264
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-02-10 19:41:22 -0300

    working on avatar component

--- src/components/atoms/Avatar/index.tsx
+++ src/components/atoms/Avatar/index.tsx
@@ -7,29 +7,20 @@ interface Props {
 }
 
 const Avatar: React.FC<Props> = ({ avatar, alt }) => {
-    return (
-        <div>
-            <AvatarStyle src={avatar} alt={alt} />
-        </div>
-    )
+    return <AvatarStyle src={avatar} alt={alt} />
 }
 
 export default Avatar
 
 export const AvatarStyle = styled.img`
-    display: flex;
-    width: 100%;
+    border-radius: 50%;
+    width: 50%;
 
-    img {
+    z-index: 10;
+
+    @media (max-width: 800px) {
+        width: 15rem;
+        height: 15rem;
         border-radius: 50%;
-        width: 50%;
-
-        z-index: 10;
-
-        @media (max-width: 800px) {
-            width: 15rem;
-            height: 15rem;
-            border-radius: 50%;
-        }
     }
 `
